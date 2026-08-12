dd7a573f27942c549af2851617cd47150e552916
Saswata Acharya <infoacaddesk@gmail.com>
2026-08-12 10:10:24 +0530

    push3

---
+++ 13.py
@@ -0,0 +1,37 @@
+student = {
+    "R1": {"name": "Amit", "dept": "CSE", "marks": 85},
+    "R2": {"name": "Rahul", "dept": "ECE", "marks": 72},
+    "R3": {"name": "Sneha", "dept": "CSE", "marks": 91},
+    "R4": {"name": "Priya", "dept": "IT", "marks": 78},
+    "R5": {"name": "Arjun", "dept": "CSE", "marks": 88}
+}
+
+# Sort h to l
+sorted_student = dict(
+    sorted(student.items(), key=lambda x: x[1]["marks"], reverse=True)
+)
+
+print("Students sorted according to marks:")
+for roll, details in sorted_student.items():
+    print(roll, details)
+
+
+# print stu with max m
+max_student = max(student.items(), key=lambda x: x[1]["marks"])
+
+print("\nStudent with maximum marks:")
+print(max_student)
+
+
+# avg m of stu
+total = sum(details["marks"] for details in student.values())
+average = total / len(student)
+
+print("\nAverage marks:", average)
+
+ 
+# stu score > avg m
+print("Students scoring more than average:")
+for roll, details in student.items():
+    if details["marks"] > average:
+        print(roll, details)
