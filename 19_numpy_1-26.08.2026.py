0fe8f2d1fd28539ef7e3c4349e2e3ac1f05ce131
Saswata Acharya <infoacaddesk@gmail.com>
2026-09-09 10:58:25 +0530

    push9

--- 19_numpy_1-26.08.2026.py
+++ 19_numpy_1-26.08.2026.py
@@ -35,3 +35,33 @@ print("5. Maximum marks subject-wise:", max_subjectwise)
 # 6. Find average marks subject-wise
 avg_subjectwise = np.mean(marks, axis=0)
 print("6. Average marks subject-wise:", avg_subjectwise)
+
+
+
+#push2 here onwards
+
+# 7. Add 10 marks to students whose Subject 1 marks are less than 50
+marks[marks[:, 1] < 50, 1] += 10
+
+print("7. Updated Marks Array:")
+print(marks)
+
+
+# 8. Count students scoring more than 80 in Subject 2
+count = np.sum(marks[:, 2] > 80)
+
+print("8. Number of students scoring more than 80 in Subject 2:", count)
+
+
+
+# 9. Minimum marks of Student 2
+student2_min = np.min(marks[1])
+
+print("9. Minimum marks of Student 2:", student2_min)
+
+
+
+# 10. Maximum marks of Student 4
+student4_max = np.max(marks[3])
+
+print("10. Maximum marks of Student 4:", student4_max)
